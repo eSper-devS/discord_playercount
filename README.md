2960d91bb2650348ee2b632af5f713960bfbcdd1
Sylvia van Os <sylvia@hackerchick.me>
2021-06-01 23:45:56 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Discord player count
+
+A very simple Discord bot for showing the player count of a Netsphere Pirates S8 server.
+
+1. Install the dependencies with `pip3 install -r requirements.txt`
+2. Create a bot and invite it to your server: https://realpython.com/how-to-make-a-discord-bot-python/
+3. Run `DISCORD_TOKEN=YOUR_TOKEN_HERE python3 main.py`
---
+++ main.py
@@ -0,0 +1,35 @@
+import asyncio
+import base64
+import json
+import os
+
+import discord
+import redis
+
+global client, player_count
+player_count = None
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
+def redis_handler(message):
+    global player_count
+    global client
+
+    data = json.loads(base64.b64decode(json.loads(message['data'])['Data']).decode('ascii'))
+    print(data)
+    if "ServerType" in data and data["ServerType"] == 3:
+        print(f"Updating player count if changed")
+        if 'Online' in data and data['Online'] != player_count:
+            player_count = data['Online']
+            print(f"Updating online count to {player_count}")
+            asyncio.run(client.change_presence(activity=discord.Game(name=f"{player_count} users online")))
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+p = r.pubsub(ignore_subscribe_messages=True)
+p.subscribe(**{'messages': redis_handler})
+p.run_in_thread(sleep_time=0.001)
+
+client.run(os.getenv('DISCORD_TOKEN'))
